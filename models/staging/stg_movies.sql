2129f2a4b5630a4b96cf2f979a1b9da19946cea1
max fishel <Max.fishel@gmail.com>
2026-08-24 16:18:37 +0000

    commiting to new branch

--- models/staging/stg_movies.sql
+++ models/staging/stg_movies.sql
@@ -2,7 +2,8 @@
     config(
         materialized='incremental',
         unique_key='movie_id',
-        incremental_strategy='merge'
+        incremental_strategy='merge',
+        on_schema_change='append_new_columns'
     )
 }}
 
